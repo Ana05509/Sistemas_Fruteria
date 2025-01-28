ddcca2295c93b312e0312aaedf65ab7162469779
Ana05509 <ana.salguero3883@utc.edu.ec>
2025-01-28 07:16:20 -0500

    cambios

--- sistema_venta_frutas/frutas/models.py
+++ sistema_venta_frutas/frutas/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    
+
     def __str__(self):
         return self.nombre
-    
+
 class Productos(models.Model):
-    codigo = models.CharField(max_length=5,primary_key=True)
+    codigo = models.CharField(max_length=5, primary_key=True)  
     nombre = models.CharField(max_length=100)
     precio = models.FloatField()
     stock = models.IntegerField()
@@ -16,15 +17,17 @@ class Productos(models.Model):
     def __str__(self):
         return self.nombre
 
+
 class Clientes(models.Model):
-    cedula = models.IntegerField()
+    cedula = models.IntegerField(unique=True)  
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
     telefono = models.CharField(max_length=10)
     email = models.EmailField()
-    
+
     def __str__(self):
         return self.nombre
+
 
 class Ventas(models.Model):
     ci_cliente = models.ForeignKey('Clientes', on_delete=models.CASCADE)
@@ -32,7 +35,6 @@ class Ventas(models.Model):
     cantidad = models.IntegerField()
     fecha = models.DateTimeField(auto_now_add=True)
     total = models.DecimalField(max_digits=10, decimal_places=2)
-    
+
     def __str__(self):
-        return self.ci_cliente.nombre
-    
+        return f"Venta de {self.ci_producto.nombre} a {self.ci_cliente.nombre}"
